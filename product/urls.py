8769a6368dd90cc821522239fac42324fcd40e45
bekbolshadiev <shba0110@gmail.com>
2021-11-11 16:08:04 +0600

    +CRUD

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 
-from .views import ProductListView
+from .views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView, \
+    SearchListView
 
 urlpatterns = [
-    path('', ProductListView.as_view()),
+    path('', ProductListView.as_view(), name='home'),
+    path('product/<int:product_id>/', ProductDetailView.as_view(), name='detail'),
+    path('product/create/', ProductCreateView.as_view(), name='product-create'),
+    path('product/update/<int:product_id>/', ProductUpdateView.as_view(),
+         name='product-update'),
+    path('product/delete/<int:product_id>/', ProductDeleteView.as_view(),
+         name='product-delete'),
+    path('search', SearchListView.as_view(), name='search'),
 
 ]
